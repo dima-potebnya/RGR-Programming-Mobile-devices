176e12ddab579219b326e86ea075bc32aeec1950
dima-potebnya <118349060+dima-potebnya@users.noreply.github.com>
2022-12-15 01:20:48 +0200

    Add files via upload

---
+++ app/src/test/java/com/example/weatherapp/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package com.example.weatherapp;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * приклад локального модульного тесту, який виконуватиметься на машині розробки (хост).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
